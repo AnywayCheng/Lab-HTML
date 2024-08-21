5ad39fbbc60c8a9809d3a15fc53d31752add737c
AnywayCheng <waynecheng981@gmail.com>
2024-08-21 14:30:09 +0800

    last

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='HTML')
 
 @app.route('/')
 def home():
     return render_template('HomePage.html')
+@app.route('/professor')
+def professor():
+    return render_template('Professor.html')
+
+@app.route('/members')
+def members():
+    return render_template('Member.html')
+
+@app.route('/thesis')
+def thesis():
+    return render_template('Thesis.html')
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True)
